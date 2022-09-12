b73b0f088f47a9cb2476ed59781b7ad2aadc9820
conelo03 <ramadhan071thea@gmail.com>
2022-09-12 16:28:14 +0700

    home link fix

--- application/views/front/template/header.php
+++ application/views/front/template/header.php
@@ -101,7 +101,7 @@
   <header id="header" class="header fixed-top">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="<?php base_url() ?>" class="logo d-flex align-items-center">
         <img src="<?= base_url('assets/front/img/logo.png') ?>" class="logo" />
         <span>Wisata Kota Beriman</span>
       </a>
